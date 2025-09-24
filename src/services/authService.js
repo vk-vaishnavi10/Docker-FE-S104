da6f473133e9653bc53a22a55ce0a273ae4a6dc5
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2025-09-24 16:24:03 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/auth";
+const API_URL = "http://backend:8081/auth"; // use docker-compose service name
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
