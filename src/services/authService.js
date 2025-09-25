c2abc891d81a0bfb080545c94b93098bf3a779ee
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2025-09-25 14:25:06 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-51-21-253-106.eu-north-1.compute.amazonaws.com:8081/auth"; // use docker-compose service name
+const API_URL = "http://ec2-13-49-69-195.eu-north-1.compute.amazonaws.com:8081/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
@@ -15,4 +15,5 @@ export const signup = async (username, email, password) => {
 export const logout = () => {
   localStorage.removeItem("token");
 };
+
 
