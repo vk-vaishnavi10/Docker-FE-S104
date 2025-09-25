432d0eff0acb06fc8b5724536fb849bce10677d8
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2025-09-25 12:28:59 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://ec2-51-21-253-106.eu-north-1.compute.amazonaws.com:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
@@ -17,3 +17,4 @@ export const getProducts = async (category = "") => {
 export const getProductImageUrl = (imagePath) => {
   return `http://localhost:8080/api/products/images/${imagePath}`;
 };
+
