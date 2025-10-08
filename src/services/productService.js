9aa6640837682a6ee5e509562a92bcaf207f066c
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2025-10-08 11:50:30 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-13-49-69-195.eu-north-1.compute.amazonaws.com:8081/api/products";
+const API_BASE_URL = "http://ec2-13-53-164-149.eu-north-1.compute.amazonaws.com:8081/api/products";
 
 export const getProducts = async (category = "") => {
   try {
@@ -13,7 +13,8 @@ export const getProducts = async (category = "") => {
 };
 
 export const getProductImageUrl = (imagePath) => {
-  return `http://ec2-13-49-69-195.eu-north-1.compute.amazonaws.com:8081/api/products/images/${imagePath}`;
+  return `http://ec2-13-53-164-149.eu-north-1.compute.amazonaws.com:8081/api/products/images/${imagePath}`;
 };
+
 
 
